aad676df64c7a7900c496de048fbdbee9e22f6d4
linguihao <5673391+linguihao@user.noreply.gitee.com>
2020-06-15 17:50:50 +0800

    初始化 veupress并配置默认主题

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+// .vuepress/config.js
+module.exports = {
+    themeConfig: {
+        logo: '/assets/img/logo.jpg',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }
+  }
+  
