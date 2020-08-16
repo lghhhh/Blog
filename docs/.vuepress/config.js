6b0f4361e7edf0573d569b036e4f37f17b69456c
小白杨 <yanglingjie@yanglingjiedeMacBook-Pro.local>
2020-08-16 22:14:21 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,20 +4,11 @@ module.exports = {
   themeConfig: {
     logo: '/logo.jpg', // 左上角logo
     nav: [
-      { text: 'Home', link: '/' },
-      { text: '博客', link: '/blog/' },
-      {
-        text: '记录', link: '/mark/',
-        items: [
-          { text: 'live', link: '/mark/live/' },
-          { text: 'book', link: '/mark/book/' }
-        ]
-      },
-      { text: '外链', link: 'http://www.bing.com', target: '_self' }
+      { text: 'Blog', link: '/blog/' },
     ],
     displayAllHeaders: true , // 默认值：false
     sidebar: {
-      '/mark/':[
+      '/blog/':[
         {
           "title": "记录",
           'path': 'live/',
