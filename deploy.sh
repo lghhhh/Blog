041f9d26f67d76e43daa30fc42bc5e2c96e99fe8
小白杨 <yanglingjie@yanglingjiedeMacBook-Pro.local>
2020-08-23 11:47:20 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/lghhhh/blog.git master:gh-pages
 git push -f https://gitee.com/linguihao/blog.git master:gh-pages
 
 
